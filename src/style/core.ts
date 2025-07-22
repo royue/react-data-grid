a3b89de333641103d43ec85cd717158919733220
Aman Mahajan <amahajan@stratag.com>
2025-07-22 09:20:53 -0500

    Add `color-scheme` selection component (#3825)
    * Add an open to select color-scheme
    
    * Add label
    
    * Rename
    
    * Simplify
    
    * Update src/style/core.ts
    
    Co-authored-by: Nicolas Stepien <567105+nstepien@users.noreply.github.com>
    
    * Add space
    
    ---------
    
    Co-authored-by: Nicolas Stepien <567105+nstepien@users.noreply.github.com>

--- src/style/core.ts
+++ src/style/core.ts
@@ -39,14 +39,33 @@ const root = css`
   }
 
   @layer rdg.Root {
-    ${lightTheme}
     --rdg-selection-width: 2px;
-    --rdg-selection-color:var(--ant-color-primary, hsl(207, 75%, 66%));
+    --rdg-selection-color: var(--ant-color-primary, hsl(207, 75%, 66%));
     --rdg-font-size: 14px;
     --rdg-cell-frozen-box-shadow: 2px 0 5px -2px rgba(136, 136, 136, 0.3);
     --rdg-cell-right-frozen-box-shadow: -2px 0 5px -2px rgba(136, 136, 136, 0.3);
     --rdg-border-width: 1px;
     --rdg-summary-border-width: calc(var(--rdg-border-width) * 2);
+    --rdg-color: light-dark(#000, #ddd);
+    --rdg-border-color: light-dark(#ddd, #444);
+    --rdg-summary-border-color: light-dark(#aaa, #555);
+    --rdg-background-color: light-dark(hsl(0deg 0% 100%), hsl(0deg 0% 13%));
+    --rdg-header-background-color: light-dark(hsl(0deg 0% 97.5%), hsl(0deg 0% 10.5%));
+    --rdg-header-draggable-background-color: light-dark(hsl(0deg 0% 90.5%), hsl(0deg 0% 17.5%));
+    --rdg-row-hover-background-color: light-dark(hsl(0deg 0% 96%), hsl(0deg 0% 9%));
+    --rdg-row-selected-background-color: light-dark(hsl(207deg 76% 92%), hsl(207deg 76% 42%));
+    --rdg-row-selected-hover-background-color: light-dark(hsl(207deg 76% 88%), hsl(207deg 76% 38%));
+    --rdg-checkbox-focus-color: hsl(207deg 100% 69%);
+
+    &.rdg-dark {
+      --rdg-color-scheme: dark;
+    }
+
+    &.rdg-light {
+      --rdg-color-scheme: light;
+    }
+
+    color-scheme: var(--rdg-color-scheme, light dark);
 
     &:dir(rtl) {
       --rdg-cell-frozen-box-shadow: -2px 0 5px -2px rgba(136, 136, 136, 0.3);
@@ -56,7 +75,10 @@ const root = css`
     display: grid;
 
     color-scheme: var(--rdg-color-scheme, light dark);
-    accent-color: light-dark(var(--ant-color-primary, hsl(207deg 100% 29%)),var(--ant-color-primary,  hsl(207deg 100% 79%)));
+    accent-color: light-dark(
+      var(--ant-color-primary, hsl(207deg 100% 29%)),
+      var(--ant-color-primary, hsl(207deg 100% 79%))
+    );
 
     /* https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context */
     /* We set a stacking context so internal elements don't render on top of external elements. */
@@ -76,21 +98,6 @@ const root = css`
       content: '';
       grid-column: 1/-1;
       grid-row: 1/-1;
-    }
-
-    &.rdg-dark {
-      --rdg-color-scheme: dark;
-      ${darkTheme}
-    }
-
-    &.rdg-light {
-      --rdg-color-scheme: light;
-    }
-
-    @media (prefers-color-scheme: dark) {
-      &:not(.rdg-light) {
-        ${darkTheme}
-      }
     }
 
     > :nth-last-child(1 of .${topSummaryRowClassname}) {
